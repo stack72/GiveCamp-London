980adb627bc783a9881fc6a0a7d75644d8654df2
Paul Stack <paulstack@hotmail.com>
2011-04-05 20:27:11 +0100

    removal of some o the old files no longer needed now that the aspnet membership role provider works

--- GiveCampLondon/Repositories/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GiveCampLondon.Repositories
-{
-    public interface IUserRepository
-    {
-        Member GetUserByUserName(string userName);
-        bool ValidateUser(string userName, string password);
-        Member CreateUser(Member newMember);
-    }
-}
