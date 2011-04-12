0601c8def1d72dbe61c7089a7cc764978be15ace
Paul Stack <paulstack@hotmail.com>
2011-04-13 00:21:32 +0100

    removal of old theme files + addition of a controller to take care of showing charities

---
+++ GiveCampLondon.Website/Controllers/SponsorsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace GiveCampLondon.Website.Controllers
+{
+    public class SponsorsController : Controller
+    {
+        //
+        // GET: /Sponsors/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
