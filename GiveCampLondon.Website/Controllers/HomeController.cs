1762ace16524e0656081f9b5dc3aa0dc7b37ead8
Paul Stack <paulstack@hotmail.com>
2011-04-14 07:06:18 +0100

    contact us page can now post back to the server - needs to do soemthing with the email for now though

--- GiveCampLondon.Website/Controllers/HomeController.cs
+++ GiveCampLondon.Website/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using GiveCampLondon.Website.Models;
 
 namespace GiveCampLondon.Website.Controllers
 {
@@ -54,6 +56,25 @@ namespace GiveCampLondon.Website.Controllers
         public ActionResult ContactUs()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Contact-Us")]
+        public ActionResult ContactUs(ContactUsViewModel vm)
+        {
+            ContactUsViewModel model;
+            try
+            {
+                //do something here to mail the details
+                ViewBag.RequestSent = true;
+                model = new ContactUsViewModel();
+            }
+            catch (Exception)
+            {
+                model = vm;
+            }
+
+            return View(model);
         }
     }
 }
---
+++ GiveCampLondon.Website/Models/ContactUsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GiveCampLondon.Website.Models
+{
+    public class ContactUsViewModel
+    {
+        public string EmailAddress { get; set; }
+        public string Name { get; set; }
+        public string Enquiry { get; set; }
+    }
+}
