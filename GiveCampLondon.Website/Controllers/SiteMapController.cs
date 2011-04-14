d133c525023c50c1b1f8cc5179ebd39b9bc08c3d
Paul Stack <paulstack@hotmail.com>
2011-04-14 06:54:58 +0100

    addition of a controller that will show the site map

---
+++ GiveCampLondon.Website/Controllers/SiteMapController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace GiveCampLondon.Website.Controllers
+{
+    public class SiteMapController : Controller
+    {
+        //
+        // GET: /SiteMap/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
