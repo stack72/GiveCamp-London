b0985d95b7ce8af43e1697884bca0a7e74ffde3a
Paul Stack <paulstack@hotmail.com>
2011-06-01 20:56:20 +0100

    more classes not used are removed as well as addition of sponsors and sitemap controller tests

---
+++ Tests/GiveCampLondon.Website.Tests/Controllers/SiteMapController.Tests.cs
@@ -0,0 +1,21 @@
+﻿using GiveCampLondon.Website.Controllers;
+using MvcContrib.TestHelper;
+using NUnit.Framework;
+
+namespace GiveCampLondon.Website.Tests.Controllers
+{
+    [TestFixture]
+    public class SiteMapControllerTests
+    {
+        [Test]
+        public void IndexAction_Returns_View()
+        {
+            //Act
+            var controller = new SiteMapController();
+            var result = controller.Index();
+
+            //Assert
+            result.AssertViewRendered();
+        }
+    }
+}
