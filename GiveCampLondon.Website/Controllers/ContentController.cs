865632e1f889a9152768634ec2cbb6e8f373c525
Paul Stack <paulstack@hotmail.com>
2011-06-23 22:30:52 +0100

    addition of the work to add the sponsors in the left hand panel as shuffled list

---
+++ GiveCampLondon.Website/Controllers/ContentController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GiveCampLondon.Repositories;
+using GiveCampLondon.Website.Helpers;
+
+namespace GiveCampLondon.Website.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly ISponsorRepository _sponsorRepository;
+        public ContentController(ISponsorRepository sponsorsRepository)
+        {
+            _sponsorRepository = sponsorsRepository;
+        }
+
+        //
+        // GET: /Content/
+        [ChildActionOnly]
+        public ActionResult RotatorContent()
+        {
+            var sponsors = GetSponsorsFromCache();
+            if (sponsors == null)
+            {
+                sponsors = _sponsorRepository.FindAll();
+                HttpRuntime.Cache.Insert("LeftHandPanelSponsors", sponsors);
+            }
+            
+            sponsors.Shuffle();
+            return PartialView(sponsors);
+        }
+
+        private IList<Sponsor> GetSponsorsFromCache()
+        {
+            if (HttpRuntime.Cache["LeftHandPanelSponsors"] != null)
+            {
+                return HttpRuntime.Cache["LeftHandPanelSponsors"] as IList<Sponsor>;
+            }
+
+            return null;
+        }
+    }
+}
--- GiveCampLondon/Repositories/SponsorRepository.cs
+++ GiveCampLondon/Repositories/SponsorRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Web.Caching;
 
 namespace GiveCampLondon.Repositories
 {
