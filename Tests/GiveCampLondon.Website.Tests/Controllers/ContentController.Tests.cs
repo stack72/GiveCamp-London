7e9d6bc79a2b27f06e5296dac3e6a19cd38c1c0a
Paul Stack <paulstack@hotmail.com>
2011-06-28 19:29:51 +0100

    addition of tests to test content controller

---
+++ Tests/GiveCampLondon.Website.Tests/Controllers/ContentController.Tests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using GiveCampLondon.Repositories;
+using GiveCampLondon.Website.Controllers;
+using GiveCampLondon.Website.Helpers;
+using MvcContrib.TestHelper;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace GiveCampLondon.Website.Tests.Controllers
+{
+    [TestFixture]
+    public class ContentControllerTests
+    {
+        private ISponsorRepository _sponsorRepository;
+        private IWaitListHelper _waitListHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sponsorRepository = Substitute.For<ISponsorRepository>();
+            _waitListHelper = Substitute.For<IWaitListHelper>();
+        }
+
+        [Test]
+        public void EventFullBanner_Action_Returns_View()
+        {
+            //Arrange
+            _waitListHelper.SetWaitListStatus().Returns(true);
+
+            //Act
+            var controller = new ContentController(_sponsorRepository, _waitListHelper);
+            var result = controller.EventFullBanner();
+            
+            //Assert
+            result.AssertPartialViewRendered();
+        }
+    
+        [Test]
+        public void RotatorContent_Action_Returns_View()
+        {
+            //Arrange
+            _sponsorRepository.FindAll().ReceivedWithAnyArgs().Returns(new List<Sponsor>());
+
+            //Act
+            var controller = new ContentController(_sponsorRepository, _waitListHelper);
+            var result = controller.RotatorContent();
+
+            //Assert
+            result.AssertPartialViewRendered();
+        }
+    }
+}
