b4730f6e6a95a36dd85df83ff025ddbbd66ca722
Paul Stack <paulstack@hotmail.com>
2011-09-13 17:48:37 +0100

    movement of the non techie volunteer functionality to a controller / repo of its own

--- GiveCampLondon.Website/Controllers/AdminController.cs
+++ GiveCampLondon.Website/Controllers/AdminController.cs
@@ -1,22 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using GiveCampLondon.Repositories;
 using GiveCampLondon.Website.Models;
-using GiveCampLondon.Website.Models.Volunteer;
 
 namespace GiveCampLondon.Website.Controllers
 {
     [Authorize(Roles = "Administrator")]
     public class AdminController : Controller
     {
-        public AdminController(IContentRepository contentRepository, IJobRoleRepository jobRoleRepository,
-         INonTechVolunteerRepository nonTechieVolunteerRepository)
+        public AdminController(IContentRepository contentRepository, IJobRoleRepository jobRoleRepository)
         {
-            _jobRoleRepository = jobRoleRepository;
-            _nonTechieVolunteerRepository = nonTechieVolunteerRepository;
             _contentRepository = contentRepository;
             _slugs = _contentRepository.GetSlugs();
             _slugSelectList = PopulateSlugDropdown();
@@ -25,8 +20,6 @@ namespace GiveCampLondon.Website.Controllers
         private readonly IContentRepository _contentRepository;
         private static List<string> _slugs;
         private static List<SelectListItem> _slugSelectList;
-        private readonly INonTechVolunteerRepository _nonTechieVolunteerRepository;
-        private readonly IJobRoleRepository _jobRoleRepository;
 
         public ActionResult ControlPanel()
         {
@@ -89,51 +82,6 @@ namespace GiveCampLondon.Website.Controllers
             return Json(content, JsonRequestBehavior.AllowGet);
         }
 
-        
-        public ActionResult NonTechies()
-        {
-            IEnumerable<NonTechieVolunteerSummaryModel> nonTechieVolunteers = _nonTechieVolunteerRepository.FindAll()
-                .Where(x => x.HasCancelled == false)
-                .Select(volunteer => new NonTechieVolunteerSummaryModel
-                {
-                    Id = volunteer.Id,
-                    LastName = volunteer.LastName,
-                    FirstName = volunteer.FirstName,
-                    Email = volunteer.Email,
-                    PhoneNumber = volunteer.PhoneNumber,
-                    TwitterHandle = volunteer.TwitterHandle
-                });
-            return View(nonTechieVolunteers);
-        }
-
-
-        public ActionResult NonTechieDetails(int id)
-        {
-            var nonTechie = _nonTechieVolunteerRepository.Get(id);
-            nonTechie.AreasOfExpertise = _nonTechieVolunteerRepository.FindExpertiseFor(nonTechie.Id);
-
-            return View(nonTechie);
-        }
-
-        [HttpPost]
-        public ActionResult NonTechieCancellation(int Id)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _nonTechieVolunteerRepository.CancelRegistration(Id);
-                    return RedirectToAction("NonTechies");
-                }
-                catch (Exception ex)
-                {
-                    ModelState.AddModelError("Error trying to deregister user", ex);
-                    return View("NonTechies");
-                }
-            }
-            return View("NonTechies");
-        }
-
         private static List<SelectListItem> PopulateSlugDropdown()
         {
             var selectList = new List<SelectListItem>();
@@ -141,5 +89,5 @@ namespace GiveCampLondon.Website.Controllers
 
             return selectList;
         }
-}
+    }
 }
---
+++ GiveCampLondon.Website/Controllers/NonTechieAdminController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GiveCampLondon.Repositories;
+using GiveCampLondon.Website.Models.Volunteer;
+
+namespace GiveCampLondon.Website.Controllers
+{
+    public class NonTechieAdminController : Controller
+    {
+        private readonly INonTechVolunteerRepository _nonTechieVolunteerRepository;
+
+        public NonTechieAdminController(INonTechVolunteerRepository nonTechieVolunteerRepository)
+        {
+            _nonTechieVolunteerRepository = nonTechieVolunteerRepository;
+        }
+
+        public ActionResult NonTechies()
+        {
+            IEnumerable<NonTechieVolunteerSummaryModel> nonTechieVolunteers = _nonTechieVolunteerRepository.FindAll()
+                .Where(x => x.HasCancelled == false)
+                .Select(volunteer => new NonTechieVolunteerSummaryModel
+                {
+                    Id = volunteer.Id,
+                    LastName = volunteer.LastName,
+                    FirstName = volunteer.FirstName,
+                    Email = volunteer.Email,
+                    PhoneNumber = volunteer.PhoneNumber,
+                    TwitterHandle = volunteer.TwitterHandle
+                });
+            return View(nonTechieVolunteers);
+        }
+
+
+        public ActionResult NonTechieDetails(int id)
+        {
+            var nonTechie = _nonTechieVolunteerRepository.Get(id);
+            nonTechie.AreasOfExpertise = _nonTechieVolunteerRepository.FindExpertiseFor(nonTechie.Id);
+
+            return View(nonTechie);
+        }
+
+        [HttpPost]
+        public ActionResult NonTechieCancellation(int Id)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _nonTechieVolunteerRepository.CancelRegistration(Id);
+                    return RedirectToAction("NonTechies");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("Error trying to deregister user", ex);
+                    return View("NonTechies");
+                }
+            }
+            return View("NonTechies");
+        }
+
+    }
+}
