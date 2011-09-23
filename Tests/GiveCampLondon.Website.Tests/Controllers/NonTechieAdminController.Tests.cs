bf7b7569bbcc6257f613574dde120dc22c97b108
Paul Stack <paulstack@hotmail.com>
2011-09-23 18:11:56 +0100

    addition of tests around the NonTechieAdmin controller

--- Tests/GiveCampLondon.Website.Tests/Controllers/NonTechieAdminController.Tests.cs
+++ Tests/GiveCampLondon.Website.Tests/Controllers/NonTechieAdminController.Tests.cs
@@ -1,11 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using GiveCampLondon.Repositories;
+using GiveCampLondon.Website.Controllers;
+using GiveCampLondon.Website.Models.Volunteer;
+using MvcContrib.TestHelper;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace GiveCampLondon.Website.Tests.Controllers
 {
-    class Class1
+    [TestFixture]
+    public class NonTechieAdminControllerTests
     {
+        private INonTechVolunteerRepository _nonTechieVolunteerRepository;
+ 
+        [SetUp]
+        public void SetUp()
+        {
+            _nonTechieVolunteerRepository = Substitute.For<INonTechVolunteerRepository>();
+        }
+
+        [Test]
+        public void NonTechies_Action_Returns_View()
+        {
+            _nonTechieVolunteerRepository.FindAll().ReceivedWithAnyArgs().Returns(new List<NonTechVolunteer>());
+
+            var controller = new NonTechieAdminController(_nonTechieVolunteerRepository);
+            var result = controller.NonTechies();
+
+            result.AssertViewRendered();
+        }
+
+        [Test]
+        public void NonTechies_Action_Returns_IEnumerable_NonTechieVolunteerSummaryModel_To_The_View()
+        {
+            _nonTechieVolunteerRepository.FindAll().ReceivedWithAnyArgs().Returns(new List<NonTechVolunteer>());
+
+            var controller = new NonTechieAdminController(_nonTechieVolunteerRepository);
+            var result = controller.NonTechies();
+
+            result.AssertViewRendered().WithViewData<IEnumerable<NonTechieVolunteerSummaryModel>>();
+        }
     }
 }
